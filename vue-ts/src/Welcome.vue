d0655a7290f6f601283e26c584074301a5493f12
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-05 23:46:53 +0900

    hello ts

---
+++ vue-ts/src/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'Welcome',
+	data() {
+		return {
+			msg: 'Hello, TypeScript'
+		}
+	}
+})
+</script>
