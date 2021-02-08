062fed30df13a319128bdc4dec25beb3e0c805e6
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-09 00:03:36 +0900

    modify slacl-clone view

--- vue-ts/src/components/Top.vue
+++ vue-ts/src/components/Top.vue
@@ -1,19 +1,43 @@
 <template>
 	<div>
-		<h1>じゃんけんゲーム</h1>
 		<div>
-			<Game />
+			<ChatPage />
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue, { defineComponent } from 'vue'
-import Game from './Game.vue'
+// import Game from './Game.vue'
+import ChatPage from '../slack_clone/ChatPage.vue'
 export default defineComponent({
 	name: 'Top',
 	components: {
-		Game
+		// Game
+		ChatPage
 	}
 })
 </script>
+
+<style>
+html {
+	height: 100%;
+	width: 100%;
+}
+
+body {
+	margin: 0;
+	height: 100%;
+	width: 100%;
+}
+
+h1 {
+	margin: 0;
+}
+
+div {
+	height: 100%;
+	width: 100%;
+}
+
+</style>
---
+++ vue-ts/src/slack_clone/ChatPage.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="slack-clone">
+		<div class="slack-clone--header">検索</div>
+		<div class="slack-clone--main">
+			<div class="slack-clone--main__channel">チャンネル</div>
+			<div class="slack-clone--main__chat">チャットスペース</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue, { defineComponent } from 'vue'
+export default defineComponent({
+	name: 'ChatPage'
+})
+</script>
+
+<style>
+.slack-clone {
+	width: 100%;
+	height: 100%;
+}
+
+.slack-clone--header {
+	height: 5%;
+	background: red;
+}
+
+.slack-clone--main {
+	height: 95%;
+	display: flex;
+	flex-direction: row;
+}
+
+.slack-clone--main__channel {
+	width: 20%;
+	background: burlywood;
+}
+
+.slack-clone--main__chat {
+	width: 80%;
+	background: cornflowerblue;
+}
+
+</style>
