9e48511a64e2d28e1889343f918e9808b26e7416
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-09 00:23:38 +0900

    add serch form

--- vue-ts/src/slack_clone/ChatPage.vue
+++ vue-ts/src/slack_clone/ChatPage.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="slack-clone">
-		<div class="slack-clone--header">検索</div>
+		<div class="slack-clone--header">
+			<form class="slack-clone--header__form">
+				<input type="text" placeholder="検索" class="slack-clone--header__form-search">
+			</form>
+		</div>
 		<div class="slack-clone--main">
 			<div class="slack-clone--main__channel">チャンネル</div>
 			<div class="slack-clone--main__chat">チャットスペース</div>
@@ -23,7 +27,21 @@ export default defineComponent({
 
 .slack-clone--header {
 	height: 5%;
-	background: red;
+	background: burlywood;
+}
+
+.slack-clone--header__form {
+	padding: 5px;
+}
+
+.slack-clone--header__form-search {
+	border-radius: 20px;
+	outline: none;
+}
+
+.slack-clone--header__form-search::placeholder {
+	position: relative;
+	left: 10px;
 }
 
 .slack-clone--main {
@@ -34,12 +52,11 @@ export default defineComponent({
 
 .slack-clone--main__channel {
 	width: 20%;
-	background: burlywood;
+	background: cornflowerblue;
 }
 
 .slack-clone--main__chat {
 	width: 80%;
-	background: cornflowerblue;
 }
 
 </style>
