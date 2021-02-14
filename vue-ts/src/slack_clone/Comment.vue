296a8585572acfad3642fa33ef4c2e4a06930495
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-14 21:27:28 +0900

    create submit comment feature

--- vue-ts/src/slack_clone/Comment.vue
+++ vue-ts/src/slack_clone/Comment.vue
@@ -1,13 +1,36 @@
 <template>
 	<div class="comment">
-		<input class="comment--input" />
+		<input type="text" class="comment--input" v-model="message"/>
+		<button class="comment--submit" @click="submitMessage">送信</button>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue, { defineComponent } from 'vue'
+import firebase from 'firebase/app';
+import 'firebase/database'
+
 export default defineComponent({
-	name: 'Comment'
+	name: 'Comment',
+	data() {
+		return {
+			message: '',
+			messages: []
+		}
+	},
+	methods: {
+		submitMessage() {
+			firebase.database().ref('comment').push({
+				comment: this.message
+			})
+			console.log(this.message)
+		}
+	},
+	mounted() {
+		firebase.database().ref('comment').on(
+			'value', snapshot => (this.messages = snapshot.val())
+		)
+	}
 })
 </script>
 
@@ -15,6 +38,9 @@ export default defineComponent({
 
 .comment {
 	height: 15%;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
 }
 
 .comment--input {
@@ -22,6 +48,12 @@ export default defineComponent({
 	width: 80%;
 	text-align: center;
 	margin: auto;
+}
+
+.comment--submit {
+	height: 20%;
+	width: 10%;
+	text-align: center;
 }
 
 </style>
